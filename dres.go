960f8e56efb6163032b9a5a47b3db67ea34cc77e
Paweł Chwała <pawel.chwala@gmail.com>
2024-01-04 23:16:43 +0100

    Delegate all queries to 1.1.1.1
    Closes #1

--- dres.go
+++ dres.go
@@ -9,9 +9,21 @@ func main() {
 		Net:  "udp",
 	}
 
-	err := server.ListenAndServe()
+	dns.HandleFunc(".", handleRequest)
 
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Unable to start dres. See error: %s", err)
+	}
+}
+
+func handleRequest(writer dns.ResponseWriter, msg *dns.Msg) {
+	response, err := dns.Exchange(msg, "1.1.1.1:53")
+	if err != nil {
+		log.Fatalf("Failed to delegate query. See error %s", err)
+	}
+
+	if writer.WriteMsg(response) != nil {
+		log.Fatalf("Unable to response. See error %s", err)
 	}
 }

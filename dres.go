91fe8419fba15ba60f898683eb3e278d3b984b42
Paweł Chwała <pawel.chwala@gmail.com>
2024-01-04 22:54:53 +0100

    Simple do-nothing resolver

--- dres.go
+++ dres.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "log"
+import "github.com/miekg/dns"
 
+func main() {
+	server := dns.Server{
+		Addr: ":53",
+		Net:  "udp",
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Unable to start dres. See error: %s", err)
+	}
 }

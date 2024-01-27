05c1665c09c2014187d2833bdf286de52dbf25ca
Paweł Chwała <pawel.chwala@gmail.com>
2024-01-27 21:00:12 +0100

    Read delegating resolver from config file (#12)
    Closes #11

--- flake.nix
+++ flake.nix
@@ -11,17 +11,44 @@
     let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
-        nixosModules.dres = { config, lib, ...}: with lib; let cfg = config.dogjam.services.dres; in {
-            options.dogjam.services.dres = {
-                enable = mkEnableOption "Enable Dres service";
-            };
+        nixosModules.dres = { config, lib, ...}:
+            with lib;
+            let
+                cfg = config.dogjam.services.dres;
+                configFile = pkgs.writeText "config.json" (builtins.toJSON cfg.config);
+            in {
+                options.dogjam.services.dres = {
+                    enable = mkEnableOption "Enable Dres service";
+                    config = mkOption {
+                        type = types.attrs;
+                        default = { };
+                        description = lib.mdDoc ''
+                            Dres configuration.
+                        '';
+                        example = literalExpression ''
+                            {
+                                resolvers = {
+                                    google = {
+                                        type = "delegating";
+                                        socket = "8.8.8.8:53";
+                                    };
+
+                                    cloudflare = {
+                                        type = "delegating";
+                                        socket = "1.1.1.1:53";
+                                    };
+                                };
+                            }
+                        '';
+                    };
+                };
 
             config = mkIf cfg.enable {
                 systemd.services."dogjam-dres" = {
                     wantedBy = [ "multi-user.target" ];
                     serviceConfig = let pkg = self.packages.x86_64-linux.default; in {
                         Restart = "on-failure";
-                        ExecStart = "${pkg}/bin/dres";
+                        ExecStart = "${pkg}/bin/dres -config ${configFile}";
                         RuntimeDirectory = "dogjam.dres";
                         RuntimeDirectoryMode = "0755";
                         StateDirectory = "dogjam.dres";

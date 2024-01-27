819a72422d0a5b9b815777616668f5cf98c6968d
Paweł Chwała <pawel.chwala@gmail.com>
2024-01-27 20:57:48 +0100

    Read delegating resolver from config file
    Closes #11

--- README.md
+++ README.md
@@ -7,48 +7,52 @@ My homelab DNS resolver.
 - [ ] configurable behavior per network's [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing)
 - [ ] support different types of resolvers:
     - [ ] [hosts file](https://en.wikipedia.org/wiki/Hosts_(file)) based resolver
-    - [ ] delegating resolver (e.g. delegating queries to [1.1.1.1](https://1.1.1.1/))
+    - [x] delegating resolver (e.g. delegating queries to [1.1.1.1](https://1.1.1.1/))
     - [ ] static host list resolver
-- [ ] available as [Nix Flake](https://nixos.wiki/wiki/Flakes)
+- [x] available as [Nix Flake](https://nixos.wiki/wiki/Flakes)
 
 # Example Configuration
 
-```yaml
-cidrs:
-  lan: 10.0.1.0/24
-  iot: 10.0.2.0/24
-  dmz: 10.0.3.0/24
-resolvers:
-  cloudflare:
-    type: delegating
-    socket: 1.1.1.1:53
-  host-file:
-    type: hosts-file
-    path: /etc/hosts
-  ad-block:
-    type: hosts-file
-    path: /etc/dres/adblock-hosts-file
-  example-com:
-    type: static
-    hosts:
-      printer.example.com: 10.0.0.20
-      media-center.example.com: 10.0.0.21
-  intra-dev:
-    type: static
-    hosts:
-      web.intra.dev: 10.0.3.20
-      db.intra.dev: 10.0.3.21
-configuration:
-  lan:
-    - host-file
-    - example-com
-    - intra-dev
-    - ad-block
-    - cloudflare
-  dmz:
-    - host-file
-    - intra-dev
-    - cloudflare
-  iot:
-    - cloudflare
+```json
+{
+  "cidrs": {
+    "lan": "10.0.1.0/24",
+    "iot": "10.0.2.0/24",
+    "dmz": "10.0.3.0/24"
+  },
+  "resolvers": {
+    "cloudflare": {
+      "type": "delegating",
+      "socket": "1.1.1.1:53"
+    },
+    "google": {
+      "type": "delegating",
+      "socket": "8.8.8.8:53"
+    },
+    "adblock": {
+      "type": "hosts-file",
+      "path": "/var/dres/adblock.hosts.file"
+    },
+    "intranet": {
+      "type": "static",
+      "hosts": {
+        "printer.example.com": "10.0.1.10",
+        "media-center.example.com": "10.0.1.20"
+      }
+    }
+  },
+  "configuration": {
+    "lan": [
+      "adblock",
+      "intranet",
+      "cloudflare"
+    ],
+    "dmz": [
+      "cloudflare"
+    ],
+    "iot": [
+      "cloudflare"
+    ]
+  }
+}
 ```
--- dres.go
+++ dres.go
@@ -1,35 +1,80 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+)
 import "github.com/miekg/dns"
 
+type DresConfig struct {
+	Resolvers map[string]ResolverConfig `json:"resolvers"`
+}
+
+type ResolverConfig struct {
+	Type   string `json:"type"`
+	Socket string `json:"socket,omitempty"`
+}
+
 func main() {
+	configFilePath := flag.String("config", "/etc/dres/config.json", "Path to JSON config file.")
+	flag.Parse()
+
+	configFile, err := os.ReadFile(*configFilePath)
+	if err != nil {
+		log.Fatalf("Unable to read config file. Error: %s", err)
+	}
+
+	var config DresConfig
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatalf("Unable to parse configuration file. Error: %s", err)
+	}
+
 	server := dns.Server{
 		Addr: ":53",
 		Net:  "udp",
 	}
 
-	dns.HandleFunc(".", handleRequest)
+	dns.HandleFunc(".", buildHandleFunc(config))
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Unable to start dres. See error: %s", err)
 	}
 }
 
-func handleRequest(writer dns.ResponseWriter, msg *dns.Msg) {
-	response, err := dns.Exchange(msg, "1.1.1.1:53")
-	log.Printf("Request from %s", writer.RemoteAddr())
-	for idx, question := range msg.Question {
-		log.Printf("  question %d: %s", idx, question.Name)
-	}
-	if err != nil {
-		log.Printf("Failed to delegate query. See error %s", err)
-		_ = writer.Close()
-		return
+func buildHandleFunc(config DresConfig) func(dns.ResponseWriter, *dns.Msg) {
+
+	var socket string
+
+	for resolverName, resolver := range config.Resolvers {
+		if resolver.Type == "delegating" {
+			socket = resolver.Socket
+			log.Printf("Found resolver named `%s` - delegating resolver, using socket %s", resolverName, socket)
+			break
+		}
 	}
 
-	if writer.WriteMsg(response) != nil {
-		log.Printf("Unable to response. See error %s", err)
+	if socket == "" {
+		log.Fatalf("Unable to find delegating resolver.")
+	}
+
+	return func(writer dns.ResponseWriter, msg *dns.Msg) {
+		response, err := dns.Exchange(msg, socket)
+		log.Printf("Request from %s", writer.RemoteAddr())
+		for idx, question := range msg.Question {
+			log.Printf("  question %d: %s", idx, question.Name)
+		}
+		if err != nil {
+			log.Printf("Failed to delegate query. See error %s", err)
+			_ = writer.Close()
+			return
+		}
+
+		if writer.WriteMsg(response) != nil {
+			log.Printf("Unable to response. See error %s", err)
+		}
 	}
 }
